c655188758a2bdfd166bd955269abd1934fda4b1
mrcaseyrobison <104058949+mrcaseyrobison@users.noreply.github.com>
2022-07-29 00:47:56 -0700

    Added JS for to render search history

--- assets/js/script.js
+++ assets/js/script.js
@@ -11,4 +11,15 @@ const searchHistory = document.getElementById("search-history");
 // Display past searches in a list
 (displaySearchHistory) => {
     searchHistory.innerHTML ="";
+
+// For loop to list array of user search history
+for (let i = userSearchHistory.length; i >= 0; i--) {
+    const btn = document.createElement('button');
+    btn.setAttribute("type", "button");
+    btn.setAttribute("today forecast");
+    btn.classList.add('history-btn');
+    btn.setAttribute('data-search', searchHistory[i]);
+    btn.textContent = userSearchHistory[i];
+    searchHistory.append[btn];
+    }
 }
